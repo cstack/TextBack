63777477bd10522ff9cdf34de064c5effb295d9c
Connor Stack <cstack@umich.edu>
2013-09-21 00:35:46 -0400

    Hello World Pebble app.

--- text_back_pebble/src/text_back_pebble.c
+++ text_back_pebble/src/text_back_pebble.c
@@ -5,18 +5,24 @@
 
 #define MY_UUID { 0xE8, 0xAE, 0x10, 0xA2, 0x2E, 0x91, 0x47, 0x3E, 0xB2, 0xFA, 0x6D, 0xD3, 0x82, 0xBA, 0xCD, 0x52 }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
+             "TextBack", "MHacks",
              1, 0, /* App version */
              DEFAULT_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
 Window window;
+TextLayer hello_layer;
 
 
 void handle_init(AppContextRef ctx) {
-
   window_init(&window, "Window Name");
   window_stack_push(&window, true /* Animated */);
+
+  text_layer_init(&hello_layer, GRect(0, 65, 144, 30));
+  text_layer_set_text_alignment(&hello_layer, GTextAlignmentCenter);
+  text_layer_set_text(&hello_layer, "Hello World!");
+  text_layer_set_font(&hello_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+  layer_add_child(&window.layer, &hello_layer.layer);
 }
 
 
